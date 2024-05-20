cb1224294649a8359844fbd81570c835a231731b
Florian Le Cossec <>
2024-05-20 10:10:42 +0200

    handle header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#FFD15B',
+        dark: '#1B1B1B',
+        grey: '#7A7A7A',
+        'light-grey': '#C6C6C6'
+      },
+      fontFamily: {
+        'sans': ['"Manrope", sans-serif'],
+        'title': ['"Anton"']
+      },
+    },
   },
   plugins: [],
 }
