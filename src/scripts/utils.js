05916fe3e74e672e8e52b9b87fbfe0a6d354750e
Florian Le Cossec <>
2024-06-28 15:33:39 +0200

    change forEach to filter loop

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -7,55 +7,23 @@ function clearSearchInput() {
     searchInput.value = '';
 }
 
+function normalizeString(str) {
+    return str.normalize('NFD')  // normalize 'nfd' permet de décomposer les caractères accentués
+        .replace(/[\u0300-\u036f]/g, '') // enlever les diacritiques (accents)
+        .replace(/[^a-zA-Z]/g, '') // enlever tout caractère non alphanumérique
+        .toLowerCase();
+}
+
 function mainSearch(query, recipes) {
-    const normalizeString = (str) => 
-        str.normalize('NFD')  // normalize 'nfd' permet de décomposer les caractères accentués
-           .replace(/[\u0300-\u036f]/g, '') // enlever les diacritiques (accents)
-           .replace(/[^a-zA-Z]/g, '') // enlever tout caractère non alphanumérique
-           .toLowerCase();
-
     const lowerCaseQuery = normalizeString(query);
-    const filteredRecipes = [];
 
-    recipes.forEach((recipe) => {
-        let match = false;
-
-        // Vérifie le nom de la recette
-        if (normalizeString(recipe.name).includes(lowerCaseQuery)) {
-            match = true;
-        }
-
-        // Vérifie les ingrédients
-        recipe.ingredients.forEach((ingredient) => {
-            if (normalizeString(ingredient.ingredient).includes(lowerCaseQuery)) {
-                match = true;
-            }
-        });
-
-        // Vérifie les ustensiles
-        recipe.ustensils.forEach((ustensil) => {
-            if (normalizeString(ustensil).includes(lowerCaseQuery)) {
-                match = true;
-            }
-        });
-
-        // Vérifie la description
-        if (normalizeString(recipe.description).includes(lowerCaseQuery)) {
-            match = true;
-        }
-
-        // Vérifie l'appareil
-        if (normalizeString(recipe.appliance).includes(lowerCaseQuery)) {
-            match = true;
-        }
-
-        // Si une correspondance est trouvée, ajoute la recette filtrée
-        if (match) {
-            filteredRecipes.push(recipe);
-        }
+    return recipes.filter((recipe) => {
+        return normalizeString(recipe.name).includes(lowerCaseQuery) ||
+            recipe.ingredients.some(ingredient => normalizeString(ingredient.ingredient).includes(lowerCaseQuery)) ||
+            recipe.ustensils.some(ustensil => normalizeString(ustensil).includes(lowerCaseQuery)) ||
+            normalizeString(recipe.description).includes(lowerCaseQuery) ||
+            normalizeString(recipe.appliance).includes(lowerCaseQuery)
     });
-
-    return filteredRecipes;
 }
 
 export { formatString, clearSearchInput, mainSearch };
